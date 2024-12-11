6778ddc4a5a538faadb74c9999ad503997869c59
Joshua Trujillo <jtriley15@gmail.com>
2024-12-11 20:08:25 +0000

    init

---
+++ src/lib.rs
@@ -0,0 +1,42 @@
+//! # Noir Runner
+//!
+//! Executes exported Noir programs. Note that programs should be exported via the `nargo export`
+//! command before running this program. Additionally, check that the `nargo` version is compatible
+//! with `v0.36.0`.
+//!
+//! ## Example
+//!
+//! ```rust
+//! use noir_runner::{NoirRunner, InputValue, FieldElement};
+//!
+//! use std::collections::BTreeMap;
+//!
+//! let program_dir = std::path::PathBuf::from("tests");
+//!
+//! let runner = NoirRunner::try_new(program_dir).unwrap();
+//!
+//! let x = FieldElement::from(2i128);
+//! let y = FieldElement::from(3i128);
+//!
+//! let input_map = BTreeMap::from([
+//!     ("x".to_owned(), InputValue::Field(x)),
+//!     ("y".to_owned(), InputValue::Field(y)),
+//! ]);
+//!
+//! let result = runner.run("addition", input_map).unwrap().unwrap();
+//!
+//! assert_eq!(result, InputValue::Field(x + y));
+//! ```
+//! 
+//! ## Re Exports
+//! 
+//! - [`FieldElement`]: (`acvm`) Represents a field element in the BN254 curve.
+//! - [`InputValue`]: (`noirc_abi`) Represents a value that can be passed as an input to a Noir program.
+
+mod error;
+mod runner;
+
+pub use acvm::FieldElement;
+pub use noirc_abi::input_parser::InputValue;
+pub use error::Error;
+pub use runner::NoirRunner;
---
+++ tests/integration-tests.rs
@@ -0,0 +1,25 @@
+use noir_runner::NoirRunner;
+use noir_runner::InputValue;
+use noir_runner::FieldElement;
+
+use std::collections::BTreeMap;
+
+#[test]
+fn test_noir_runner() {
+    let program_dir = std::path::PathBuf::from("tests");
+    let runner = NoirRunner::try_new(program_dir).unwrap();
+
+    let x = FieldElement::from(2i128);
+    let y = FieldElement::from(3i128);
+
+    let input_map = BTreeMap::from([
+        ("x".to_owned(), InputValue::Field(x)),
+        ("y".to_owned(), InputValue::Field(y)),
+    ]);
+
+    let result = runner.run("addition", input_map).unwrap().unwrap();
+
+    let expected = FieldElement::from(5i128);
+
+    assert_eq!(result, InputValue::Field(expected));
+}
